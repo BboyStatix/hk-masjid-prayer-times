08fed321de6d55a1e6a5f56d5df000f67cc61e1f
Naveed Elahi <statix231@hotmail.com>
2023-05-30 20:52:14 +0800

    basic UI layout of masjids with salah+iqamah times

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,109 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface PrayerTime {
+  date: string;
+  hijri_date: string;
+  hijri_month: string;
+  salah: {
+    fajr: string;
+    sunrise: string;
+    zuhr: string;
+    asr: string;
+    maghrib: string;
+    isha: string;
+  };
+  iqamah: {
+    fajr: string;
+    zuhr: string;
+    asr: string;
+    maghrib: string;
+    isha: string;
+    jummah1: string;
+    jummah2: string;
+    jummah3: string;
+  };
 }
+
+interface Masjid {
+  id: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  zipcode: string;
+  phone: string;
+  meta: string;
+  email: string;
+  website_url: string;
+  event_url: string;
+  donation_url: string;
+  audio_url: string;
+  prayer_timing_url: string;
+  app_premium_features: string;
+  premium_features: {
+    app: boolean;
+    slideshow: boolean;
+  };
+  is_shell_account: boolean;
+  distance: string;
+  logo: string;
+  times: PrayerTime[];
+}
+
+interface MasjidAlResponse {
+  items: Masjid[];
+}
+
+const currentDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
+
+async function fetchMasjidsInformation(): Promise<Masjid[]> {
+  const res = await fetch(
+    `http://masjidal.com/api/v3/masjids/proximity?date_start=${currentDate()}&distance=30&expand=times&lat=22.44519279770756&long=114.0417109256271`
+  );
+  if (!res.ok) {
+    throw new Error("Error occurred when fetching masjids");
+  }
+  const json = (await res.json()) as MasjidAlResponse;
+  return json.items;
+}
+
+const Masjids = async () => {
+  const masjids = await fetchMasjidsInformation();
+
+  return (
+    <div>
+      <h1 className="text-3xl">HK Masjid Prayer times</h1>
+      {masjids.map((masjid: Masjid) => (
+        <div key={masjid.id}>
+          <h2 className="text-2xl">{masjid.name}</h2>
+
+          <h3 className="text-xl">Salah</h3>
+          <div>Fajr {masjid.times[0].salah.fajr}</div>
+          <div>Sunrise {masjid.times[0].salah.sunrise}</div>
+          <div>Zuhr {masjid.times[0].salah.zuhr}</div>
+          <div>Asr {masjid.times[0].salah.asr}</div>
+          <div>Maghrib {masjid.times[0].salah.maghrib}</div>
+          <div>Isha {masjid.times[0].salah.isha}</div>
+
+          <h3 className="text-xl">Iqamah</h3>
+          <div>Fajr {masjid.times[0].iqamah.fajr}</div>
+          <div>Zuhr {masjid.times[0].iqamah.zuhr}</div>
+          <div>Asr {masjid.times[0].iqamah.asr}</div>
+          <div>Maghrib {masjid.times[0].iqamah.maghrib}</div>
+          <div>Isha {masjid.times[0].iqamah.isha}</div>
+
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Masjids;

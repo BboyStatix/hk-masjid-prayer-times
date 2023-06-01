166cc55ce30f1bcf7ee653e4a43cdab21549a8f7
Naveed Elahi <statix231@hotmail.com>
2023-06-01 19:34:53 +0800

    show mosques in a grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,30 @@ const Masjids = async () => {
   return (
     <div>
       <h1 className="text-3xl">Hong Kong Masjid Prayer times</h1>
-      {masjids.map((masjid) => (
-        <div key={masjid.id}>
-          <h2 className="text-2xl">{masjid.name}</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {masjids.map((masjid) => (
+          <div key={masjid.id} className="border">
+            <h2 className="text-2xl">{masjid.name}</h2>
 
-          <h3 className="text-xl">Salah</h3>
-          <div>Fajr {masjid.times[0].salah.fajr}</div>
-          <div>Sunrise {masjid.times[0].salah.sunrise}</div>
-          <div>Zuhr {masjid.times[0].salah.zuhr}</div>
-          <div>Asr {masjid.times[0].salah.asr}</div>
-          <div>Maghrib {masjid.times[0].salah.maghrib}</div>
-          <div>Isha {masjid.times[0].salah.isha}</div>
+            <h3 className="text-xl">Salah</h3>
+            <div>Fajr {masjid.times[0].salah.fajr}</div>
+            <div>Sunrise {masjid.times[0].salah.sunrise}</div>
+            <div>Zuhr {masjid.times[0].salah.zuhr}</div>
+            <div>Asr {masjid.times[0].salah.asr}</div>
+            <div>Maghrib {masjid.times[0].salah.maghrib}</div>
+            <div>Isha {masjid.times[0].salah.isha}</div>
 
-          <h3 className="text-xl">Iqamah</h3>
-          <div>Fajr {masjid.times[0].iqamah.fajr}</div>
-          <div>Zuhr {masjid.times[0].iqamah.zuhr}</div>
-          <div>Asr {masjid.times[0].iqamah.asr}</div>
-          <div>Maghrib {masjid.times[0].iqamah.maghrib}</div>
-          <div>Isha {masjid.times[0].iqamah.isha}</div>
+            <h3 className="text-xl">Iqamah</h3>
+            <div>Fajr {masjid.times[0].iqamah.fajr}</div>
+            <div>Zuhr {masjid.times[0].iqamah.zuhr}</div>
+            <div>Asr {masjid.times[0].iqamah.asr}</div>
+            <div>Maghrib {masjid.times[0].iqamah.maghrib}</div>
+            <div>Isha {masjid.times[0].iqamah.isha}</div>
 
-          <br />
-        </div>
-      ))}
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

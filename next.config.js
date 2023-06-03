483d288ddce44f5d88bb586d7936b41e8f508254
Naveed Elahi <statix231@hotmail.com>
2023-06-04 06:33:35 +0800

    fix image not being loaded on prod

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "masjidal.com",
-        pathname: "/uploads/**",
-      },
-    ],
+    domains: ["masjidal.com"],
   },
 };
 

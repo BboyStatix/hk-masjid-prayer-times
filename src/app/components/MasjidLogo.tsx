b9a3e9d443b48cf739c9d9dcf5cdc3d906f67957
BboyStatix <statix231@hotmail.com>
2025-11-10 11:58:05 +0800

    remove error due to passing empty string as props

--- src/app/components/MasjidLogo.tsx
+++ src/app/components/MasjidLogo.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
-const MasjidLogo = ({ name, url }: { name: string; url: string }) => (
-  <Image src={url} alt={`${name} logo`} width={100} height={100} />
-);
+const MasjidLogo = ({ name, url }: { name: string; url: string }) => {
+  if (!url) return null;
+
+  return <Image src={url} alt={`${name} logo`} width={100} height={100} />;
+};
 
 export default MasjidLogo;
